ce4921b2e7c6ba40435565ab2085efc7174bbe6a
wadakousuke <100855797+wadakousuke@users.noreply.github.com>
2022-03-21 06:36:44 +0000

    [Add コントローラーの追加]

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
+
+
   root to: "homes#top"
   get "homes/about" => "homes#about", as:"about"
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :books, only: [:new, :show, :index, :destroy, :edit]
+  resources :user, only: [:show, :edit]
 end

f48b004f14b5aa50b11e731662e2231b3f117468
wadakousuke <100855797+wadakousuke@users.noreply.github.com>
2022-03-22 15:35:52 +0000

    [Update ビューファイルの修正]

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,12 +1,22 @@
 class UsersController < ApplicationController
   def index
+      @users = User.all
   end
 
   def show
+    @user = User.find(params[:id])
+    @books = @user.books
   end
 
   def edit
+       @user = User.find(params[:id])
   end
 
 
+private
+
+def user_params
+  params.require(:user).permit(:name, :profile_image)
 end
+end
+

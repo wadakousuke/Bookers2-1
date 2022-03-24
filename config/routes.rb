55872310ce1f9f2492ec7e61a21b65bb5a5dd81c
wadakousuke <100855797+wadakousuke@users.noreply.github.com>
2022-03-24 13:12:42 +0000

    [Update User info ビューの修正 ]

--- config/routes.rb
+++ config/routes.rb
@@ -6,5 +6,5 @@ Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :books, only: [:new, :show, :index, :destroy, :edit, :create]
-  resources :users, only: [:show, :edit, :index]
+  resources :users, only: [:show, :edit, :index, :update]
 end
